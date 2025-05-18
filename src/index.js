621ebc35f8bd79aa48b3432d0a472c76a8e7f990
LAPTOP-51SS33JD\satyendra <satyendrasatya447@gmail.com>
2025-05-18 13:30:12 -0700

    added base name as repo name

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom'; // <-- Import BrowserRouter
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter> {/* <-- Wrap App in BrowserRouter */}
+    <BrowserRouter basename="/cash-balancing-app"> {/* <-- Wrap App in BrowserRouter */}
       <App />
     </BrowserRouter>
   </React.StrictMode>
